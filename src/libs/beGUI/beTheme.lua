bcee76043e0e2931238665b2efe7467d53911209
Wang Renxin <hellotony521@qq.com>
2021-10-27 14:40:28 +0800

    *Updated theme.

--- src/libs/beGUI/beTheme.lua
+++ src/libs/beGUI/beTheme.lua
@@ -21,7 +21,7 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ]]
 
-local default = function ()
+local function default()
 	local font_ = Font.new('fonts/ascii 8x8.png', Vec2.new(8, 8))
 
 	return {
